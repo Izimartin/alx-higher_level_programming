4851766afa2f3ec1f24d3d49057aa86f20f61b88
Izimartin <izimartinz@gmail.com>
2022-06-28 11:46:13 +0100

    Print Alpha

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Author - Tolulope Fakunle
+# Author - Martins Akhivbareme
+
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
